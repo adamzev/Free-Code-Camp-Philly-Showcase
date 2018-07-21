5245c22de2f7fbf5b5f47aa523243b9930527dc3
Adrian de Leon <adeleon@linode.com>
2018-07-21 05:26:46 -0400

    clean up comments

--- showcase/src/Members.js
+++ showcase/src/Members.js
@@ -3,26 +3,17 @@ import UserCard from './components/UserCard'
 import data from './data/data.js'
 
 const Members = () => {
-  return (
-    // <UserCard
-    //   image="http://via.placeholder.com/180x320"
-    //   username="Test User"
-    //   tagline="Came for the code, stayed for the code"
-    //   bio="I am a human being. No really I am totally not a computer"
-    //   projects={['project1', 'project2', 'project3']}
-    // />
-    data.map(({ image, username, tagline, bio, projects }) => {
-      return (
-        <UserCard
-          image={image}
-          username={username}
-          tagline={tagline}
-          bio={bio}
-          projects={projects}
-        />
-      )
-    })
-  )
+  return data.map(({ image, username, tagline, bio, projects }) => {
+    return (
+      <UserCard
+        image={image}
+        username={username}
+        tagline={tagline}
+        bio={bio}
+        projects={projects}
+      />
+    )
+  })
 }
 
 export default Members
