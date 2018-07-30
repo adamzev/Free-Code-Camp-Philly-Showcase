912ff3eff3f5ab516132cf3b0c654382a83c45cd
Adrian de Leon <adeleon@linode.com>
2018-07-30 09:47:23 -0400

    refactor to use one data file per user

---
+++ showcase/src/data/user2.js
@@ -0,0 +1,7 @@
+export const user2 = {
+  image: 'http://via.placeholder.com/180x320',
+  username: 'test user 2',
+  tagline: 'Came for the nice js ecosystem',
+  bio: 'I am a human being. No really, I am totally not a computer',
+  projects: ['project1', 'project2', 'project3'],
+}
