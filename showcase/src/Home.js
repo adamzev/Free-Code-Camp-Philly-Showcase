5184d376bd4e55f9984e6a0735db4c5c846f541c
Adrian de Leon <ajdeleon1064@gmail.com>
2018-08-27 20:17:40 -0400

    move <Divider> into FCCHeader and remove from individual pages

--- showcase/src/Home.js
+++ showcase/src/Home.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from 'react'
 
-import { Container, Divider } from 'semantic-ui-react';
-import ShortUserCard from './components/ShortUserCard';
+import { Container } from 'semantic-ui-react'
+import ShortUserCard from './components/ShortUserCard'
 
 const Home = () => (
   <Container>
-    <Divider></Divider>
     <h3>Approve Users</h3>
     <ShortUserCard />
   </Container>
 )
 
-export default Home;
+export default Home
