3b7a2020324eb910f4e15929eabda9793ac65406
Angel <angelr1076@gmail.com>
2018-12-01 13:17:25 -0500

    adding angel rodriguez to portfolio

--- showcase/src/data/dataIndex.js
+++ showcase/src/data/dataIndex.js
@@ -2,9 +2,11 @@ import user1 from './user1'
 import { user2 } from './user2.js'
 import user3 from './user3.js'
 import user4 from './user4.js'
+import rodriguezangel from './rodriguezangel.js'
+
 
 const data = {
-  users: [user1, user2, user3, user4],
+  users: [user1, user2, user3, user4, rodriguezangel],
   allUsers: function() {
     return this.users
   },
