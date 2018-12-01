e1aeae9767c81a8122b19c0a9d5ba386f7d15b78
Equinox <scarras86@gmail.com>
2018-12-01 13:10:58 -0500

    added jpg and js

---
+++ showcase/src/data/userSC.js
@@ -0,0 +1,22 @@
+import Steve from '../assets/StephenC.jpg';
+export default {
+  id: 4,
+  image: Steve,
+  username: 'Stephen Carrasquillo (Equinox) ',
+  tagline: 'Aspiring Computer Engineer',
+  bio: 'Coding has always been a passion. I am always looking for projects that have potential to create new paradigms, and encourage others to learnand code through impactful code.',
+  projects:[  
+    {
+      header: 'My GitHub Page',
+      image: 'https://assets-cdn.github.com/images/modules/open_graph/github-mark.png',
+      description: 'Github page with completed project',
+      href: 'http://github.com/Equinox86',
+    },
+    {
+      header: 'Current Projects',
+      image: 'https://getthedrift.com/wp-content/uploads/2018/09/Nothing1.jpg',
+      description: 'Nothing to see here',
+      href: 'google.com',
+    }
+  ],
+}
