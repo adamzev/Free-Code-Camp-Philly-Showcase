b449efbbb4822e597f469f46ddf69de780a7d3f1
Daniel Mulvihill <dmulvihill3@gmail.com>
2018-12-01 13:36:19 -0500

    made profile; intial commit

---
+++ showcase/src/data/danmulvihill.js
@@ -0,0 +1,28 @@
+export default {
+    id: 25,
+    image: 'https://media.licdn.com/dms/image/C4E03AQHpMaZ1FbEUXw/profile-displayphoto-shrink_200_200/0?e=1548892800&v=beta&t=iJZgshyJ6e3EW98VBiNd4pvgeIQr2yLyhggDtEGrhKc',
+    username: 'danmulvihill',
+    tagline: 'Front-end Developer.',
+    bio:
+      'I am new but good. Mostly Front-end (I love vanilla JS)',
+    projects: [
+      {
+        header: 'Shopping Buddy',
+        href: 'https://react-shopping-buddy.firebaseapp.com/',
+        description:
+          'Am app I started at NYCDA coding bootcamp. This version was created with React-JS and is a recreation of an earlier verion I originally made with vanilla JS. ',
+      },
+      {
+        header: 'Other project',
+        href: 'https://danmulvihill.github.io/',
+        description: 'For list of our fun apps and websites, see my one-page profile',
+      },
+      {
+        header: 'Peter Wentz Homestead',
+        href: 'http://embraceyourinnerengineer.org/peterwentz/',
+        description:
+          'I used to be a free-lance WordPress developer. Here\'s a site did for a historical site near Philadelphia, PA',
+      },
+    ],
+  }
+  
--- showcase/src/data/dataIndex.js
+++ showcase/src/data/dataIndex.js
@@ -2,9 +2,10 @@ import user1 from './user1'
 import { user2 } from './user2.js'
 import user3 from './user3.js'
 import user4 from './user4.js'
+import danmulvihill from './danmulvihill.js'
 
 const data = {
-  users: [user1, user2, user3, user4],
+  users: [user1, user2, user3, user4, danmulvihill],
   allUsers: function() {
     return this.users
   },
