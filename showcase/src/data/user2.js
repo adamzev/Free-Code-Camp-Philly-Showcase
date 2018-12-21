929551ca8bf194f5c64e3a6d15450c46e390bf40
Adrian de Leon <ajdeleon1064@gmail.com>
2018-12-20 22:54:35 -0500

    add id as dictated by new test

--- showcase/src/data/user2.js
+++ showcase/src/data/user2.js
@@ -1,28 +1,35 @@
-import jon from '../assets/jon.png';
+import jon from '../assets/jon.png'
 export const user2 = {
+  id: 2,
   image: jon,
   username: 'jon.gomez',
-  tagline: 'Junior Web Developer, passionate about collaborating with others to create innovative technological solutions to pressing issues.',
-  bio: 'I am relatively new to web development and seeking opportunities to work with others. Check out my projects!',
+  tagline:
+    'Junior Web Developer, passionate about collaborating with others to create innovative technological solutions to pressing issues.',
+  bio:
+    'I am relatively new to web development and seeking opportunities to work with others. Check out my projects!',
   projects: [
     {
       header: 'In Progress: Lifelong Learning Blog',
-      description: 'This is a blog website, currently in progress, that uses Bootstraps latest masonry layout to give blog posts a more visually appealing layout.',
+      description:
+        'This is a blog website, currently in progress, that uses Bootstraps latest masonry layout to give blog posts a more visually appealing layout.',
       href: 'https://gomezjonathan46.github.io/Life-Long-Learning-Blog/',
     },
     {
       header: 'Matching Game',
-      description: 'This is a matching card game that uses Vanilla JavaScript to respond to user events.',
+      description:
+        'This is a matching card game that uses Vanilla JavaScript to respond to user events.',
       href: 'https://gomezjonathan46.github.io/Memory-Game-Project/',
     },
     {
       header: 'Pixel Art Maker',
-      description: 'This is a pixel art maker, which uses jQuery to manipulate the website and respond to user events.',
+      description:
+        'This is a pixel art maker, which uses jQuery to manipulate the website and respond to user events.',
       href: 'https://gomezjonathan46.github.io/Pixel-Art-Maker/',
     },
     {
       header: 'Animal Trading Card',
-      description: 'This is an animal trading card created using HTML5 and CSS3.',
+      description:
+        'This is an animal trading card created using HTML5 and CSS3.',
       href: 'https://gomezjonathan46.github.io/Animal-Trading-Card/',
     },
     {
